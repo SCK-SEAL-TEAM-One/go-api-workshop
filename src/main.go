dd000964883b031be3dd78ba73f9098580de9ede
natshenghou <dai_00333@hotmail.com>
2018-07-05 16:28:03 +0700

    [Edited] เพิ่มการใช้งาน Post ค่าผ่าน json

--- src/main.go
+++ src/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type RequestBody struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
 
 func main() {
 	http.HandleFunc("/", helloworld)
@@ -21,7 +27,27 @@ func helloworld(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "hello world")
 	}
 	if r.Method == http.MethodPost {
+		query := r.URL.Query()
+		text := query.Get("text")
+		if text != "" {
+			fmt.Fprint(w, "hello ", text)
+			return
+		}
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "don't use post method")
+	}
+}
+
+func HelloWithName(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var requestBody RequestBody
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&requestBody)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprint(w, "hello ", requestBody.Name)
 	}
 }
--- src/main_test.go
+++ src/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
@@ -44,6 +46,27 @@ func Test_helloWorld_Method_Get_Should_Be_hello_golf(t *testing.T) {
 	helloworld(w, req)
 	expectedStatusCode := 200
 	expectedResponseText := "hello golf"
+
+	response := w.Result()
+	actualResponseText, _ := ioutil.ReadAll(response.Body)
+
+	if expectedStatusCode != response.StatusCode {
+		t.Errorf("should be %d but is %d", expectedStatusCode, response.StatusCode)
+	}
+	if expectedResponseText != string(actualResponseText) {
+		t.Errorf("should be \n%s' but is \n%s'", expectedResponseText, actualResponseText)
+	}
+}
+
+func Test_HelloWithName_Method_Post_Should_Be_hello_Apipol_Sukgler(t *testing.T) {
+	body := RequestBody{Id: "1", Name: "Apipol Sukgler"}
+	bodyByte, _ := json.Marshal(body)
+
+	req := httptest.NewRequest("POST", "/?text=golf", bytes.NewBuffer(bodyByte))
+	w := httptest.NewRecorder()
+	HelloWithName(w, req)
+	expectedStatusCode := 200
+	expectedResponseText := "hello Apipol Sukgler"
 
 	response := w.Result()
 	actualResponseText, _ := ioutil.ReadAll(response.Body)
